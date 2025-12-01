19989a99481d60737befaf847d21a6a2f8e0664e
Timotius Avaro Andrelo Putra <timotiusavaro@gmail.com>
2025-12-01 21:41:55 +0700

    ilangin eror reload

--- src/App.jsx
+++ src/App.jsx
@@ -33,16 +33,7 @@ function App() {
   };
   // -------------------------
 
-  useEffect(() => {
-    const isReload =
-      performance.getEntriesByType("navigation")[0]?.type === "reload";
-
-    if (isReload) {
-      // Ambil path tanpa hash
-      const baseUrl = window.location.origin + "/portofolio/";
-      window.location.replace(baseUrl);
-    }
-  }, []);
+  
 
   useEffect(() => {
     const observer = new IntersectionObserver(
